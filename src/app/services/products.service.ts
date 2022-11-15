95dd676b5846f9da95a76eb1599d7c62cee15d45
ivanbielozertsev <90478135+ivanbielozertsev@users.noreply.github.com>
2022-11-15 14:55:44 +0100

    First commit

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpParams,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { catchError, delay, Observable, retry, throwError } from 'rxjs';
+import { IProduct } from '../models/product';
+import { ErrorService } from './error.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  constructor(private http: HttpClient, private errorService: ErrorService) {}
+
+  getAll(): Observable<IProduct[]> {
+    return this.http
+      .get<IProduct[]>('https://fakestoreapi.com/products', {
+        params: new HttpParams().append('limit', 5),
+        // {fromString: 'limit=5'}
+        // { fromObject: { limit: 5 } }
+      })
+      .pipe(delay(200), retry(2), catchError(this.errorHandler.bind(this)));
+  }
+  private errorHandler(error: HttpErrorResponse) {
+    this.errorService.handle(error.message);
+    return throwError(() => error.message);
+  }
+}
