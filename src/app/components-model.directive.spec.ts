1be6a9089a5a8accfe6bb463895b2817a7132d33
ivanbielozertsev <90478135+ivanbielozertsev@users.noreply.github.com>
2022-11-15 18:04:25 +0100

    Final

---
+++ src/app/components-model.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ComponentsModelDirective } from './components-model.directive';
+
+describe('ComponentsModelDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ComponentsModelDirective();
+    expect(directive).toBeTruthy();
+  });
+});
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -4,7 +4,7 @@ import {
   HttpParams,
   HttpErrorResponse,
 } from '@angular/common/http';
-import { catchError, delay, Observable, retry, throwError } from 'rxjs';
+import { catchError, delay, Observable, retry, throwError, tap } from 'rxjs';
 import { IProduct } from '../models/product';
 import { ErrorService } from './error.service';
 
@@ -14,6 +14,8 @@ import { ErrorService } from './error.service';
 export class ProductsService {
   constructor(private http: HttpClient, private errorService: ErrorService) {}
 
+  products: IProduct[];
+
   getAll(): Observable<IProduct[]> {
     return this.http
       .get<IProduct[]>('https://fakestoreapi.com/products', {
@@ -21,8 +23,20 @@ export class ProductsService {
         // {fromString: 'limit=5'}
         // { fromObject: { limit: 5 } }
       })
-      .pipe(delay(200), retry(2), catchError(this.errorHandler.bind(this)));
+      .pipe(
+        delay(200),
+        retry(2),
+        tap((products) => (this.products = products)),
+        catchError(this.errorHandler.bind(this))
+      );
   }
+
+  create(product: IProduct): Observable<IProduct> {
+    return this.http
+      .post<IProduct>('https://fakestoreapi.com/products', product)
+      .pipe(tap((prod) => this.products.push(prod)));
+  }
+
   private errorHandler(error: HttpErrorResponse) {
     this.errorService.handle(error.message);
     return throwError(() => error.message);
